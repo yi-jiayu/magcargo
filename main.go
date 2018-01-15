cb1bb7be2233c2fbd78b38b3551483231542e428
Jiayu Yi <yi-jiayu@users.noreply.github.com>
2018-01-15 13:33:54 +0800

    Rewrite main.go to use cli flags

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"encoding/binary"
+	"crypto/rand"
+	"encoding/base64"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 	"path/filepath"
 
 	"github.com/boltdb/bolt"
@@ -13,117 +14,153 @@ import (
 )
 
 var (
-	db   *bolt.DB
-	salt string
-	hid  *hashids.HashID
+	host = flag.String("host", "", "Hostname")
+	port = flag.String("port", "8080", "Port to listen on")
+
+	salt      = flag.String("salt", "", "Salt for generating short URLs")
+	dbPath    = flag.String("db", "magcargo.db", "Path to database file")
+	minLength = flag.Int("minlength", 5, "Minimum length for short URLs")
 )
 
-// itob returns an 8-byte big endian representation of v.
-func itob(v int) []byte {
-	b := make([]byte, 8)
-	binary.BigEndian.PutUint64(b, uint64(v))
-	return b
-}
-
-func newShortURL(url string) (id int, err error) {
-	err = db.Update(func(tx *bolt.Tx) error {
-		b, err := tx.CreateBucketIfNotExists([]byte("urls"))
+func shortenURL(db *bolt.DB, hid *hashids.HashID, salt, URL string) (short string, err error) {
+	err = db.Update(func(tx *bolt.Tx) (err error) {
+		// open a bucket namespaced to salt
+		b, err := tx.CreateBucketIfNotExists([]byte(salt))
 		if err != nil {
-			return err
+			return
 		}
 
-		next, _ := b.NextSequence()
-		id = int(next)
+		// get the next sequence number
+		next, err := b.NextSequence()
+		if err != nil {
+			return
+		}
 
-		return b.Put(itob(id), []byte(url))
+		// convert it to a hashid
+		s, err := hid.EncodeInt64([]int64{int64(next)})
+		if err != nil {
+			return
+		}
+
+		// save it to the db
+		err = b.Put([]byte(s), []byte(URL))
+		if err != nil {
+			return
+		}
+
+		// update return value
+		short = s
+		return
 	})
 
-	return id, err
+	return
 }
 
-func idToString(id int) string {
-	s, _ := hid.Encode([]int{id})
-	return s
-}
-
-func getShortURL(id int) (url string) {
-	_ = db.View(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte("urls"))
+func unshortenURL(db *bolt.DB, salt, short string) (URL string) {
+	_ = db.View(func(tx *bolt.Tx) (err error) {
+		b := tx.Bucket([]byte(salt))
 		if b == nil {
-			return nil
+			return
 		}
 
-		burl := b.Get(itob(id))
-		url = string(burl)
-		return nil
+		long := b.Get([]byte(short))
+		URL = string(long)
+		return
 	})
 
-	return url
+	return
 }
 
-func stringToID(short string) (id int, err error) {
-	d, err := hid.DecodeWithError(short)
+func createHandler(db *bolt.DB, hid *hashids.HashID, salt string) http.Handler {
+	handler := func(res http.ResponseWriter, req *http.Request) {
+		switch req.Method {
+		case http.MethodGet:
+			short := req.URL.Path[1:]
+			URL := unshortenURL(db, salt, short)
+			if URL == "" {
+				http.NotFound(res, req)
+				return
+			}
+
+			http.Redirect(res, req, URL, http.StatusSeeOther)
+		case http.MethodPost:
+			err := req.ParseForm()
+			if err != nil {
+				log.Println(err)
+				res.WriteHeader(http.StatusBadRequest)
+				return
+			}
+
+			URL := req.Form.Get("url")
+			if URL == "" {
+				res.WriteHeader(http.StatusBadRequest)
+				return
+			}
+
+			short, err := shortenURL(db, hid, salt, URL)
+			if err != nil {
+				log.Println(err)
+				res.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+
+			res.WriteHeader(http.StatusCreated)
+			res.Write([]byte(short))
+		}
+	}
+
+	return http.HandlerFunc(handler)
+}
+
+func generateRandomSalt(length int) (salt string, err error) {
+	b := make([]byte, length)
+	_, err = rand.Read(b)
 	if err != nil {
-		return 0, err
+		return
 	}
 
-	return d[0], nil
-}
-
-func ServeHTTP(res http.ResponseWriter, req *http.Request) {
-	switch req.Method {
-	case http.MethodGet:
-		short := req.URL.Path[1:]
-		id, err := stringToID(short)
-		if err != nil {
-			log.Println(err)
-			res.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		url := getShortURL(id)
-		if url == "" {
-			http.NotFound(res, req)
-			return
-		}
-
-		http.Redirect(res, req, url, http.StatusSeeOther)
-	case http.MethodPost:
-		_ = req.ParseForm()
-		url := req.Form.Get("url")
-		id, err := newShortURL(url)
-		if err != nil {
-			log.Println(err)
-			res.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-		short := idToString(id)
-		res.WriteHeader(http.StatusCreated)
-		res.Write([]byte(short))
-	}
+	salt = base64.StdEncoding.EncodeToString(b)
+	return
 }
 
 func main() {
-	exe, err := os.Executable()
+	flag.Parse()
+
+	if *salt == "" {
+		fmt.Println("Generating random salt...")
+
+		// the number of bytes to generate the salt was arbitrarily decided
+		randSalt, err := generateRandomSalt(12)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		*salt = randSalt
+		fmt.Println("Using random salt: " + randSalt)
+	}
+
+	dbPathAbs, err := filepath.Abs(*dbPath)
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println("Using database file at: " + dbPathAbs)
 
-	cwd := filepath.Dir(exe)
-	fmt.Println("Current directory: " + cwd)
-
-	db, err = bolt.Open("magcargo.db", 0600, nil)
+	db, err := bolt.Open(dbPathAbs, 0600, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
-	salt = "slugma magcargo swinub"
 	hd := hashids.NewData()
-	hd.Salt = salt
-	hd.MinLength = 5
-	hid, _ = hashids.NewWithData(hd)
+	hd.Salt = *salt
+	hd.MinLength = *minLength
+	hid, err := hashids.NewWithData(hd)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	log.Fatal(http.ListenAndServe(":8080", http.HandlerFunc(ServeHTTP)))
+	handler := createHandler(db, hid, *salt)
+	addr := fmt.Sprintf("%s:%s", *host, *port)
+	fmt.Println("Listening on " + addr)
+	log.Fatal(http.ListenAndServe(addr, handler))
 }
